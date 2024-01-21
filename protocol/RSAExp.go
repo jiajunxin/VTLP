d6f70709946bdb61ba3a96a396e16c9fd7247d06
Jiajun Xin <jiajun_xin@outlook.com>
2024-01-21 23:13:22 +0800

    add RSA setting for sig/VRF

--- protocol/RSAExp.go
+++ protocol/RSAExp.go
@@ -5,36 +5,48 @@ import (
 	"math/big"
 )
 
-// RSAExpProof contains the public/private parts of an RSA key
+// RSAExpProof contains the public/private parts of an RSA key, RSAMod = P*Q, D=publicKey(default 17), D*E = 1 mod phi(N)
 type RSAExpProof struct {
-	RSAMod big.Int
-	Base   big.Int
-	P      big.Int
-	Q      big.Int
+	RSAMod *big.Int
+	Base   *big.Int
+	P      *big.Int
+	Q      *big.Int
+	D      *big.Int
+	E      *big.Int
 	//Order is the order of subgroup QR_N
-	Order    big.Int
-	Exponent big.Int // Exponent  = 2^TimePara mod Order
+	Order    *big.Int
+	Exponent *big.Int // Exponent  = 2^TimePara mod Order
 }
 
 func RSAExpSetup() *RSAExpProof {
 	var ret RSAExpProof
 	// ret.P = *getSafePrime()
 	// ret.Q = *getSafePrime()
+	ret.P = new(big.Int)
+	ret.Q = new(big.Int)
 	ret.P.SetString(PString, 10)
 	ret.Q.SetString(Qstring, 10)
-	ret.RSAMod.Mul(&ret.P, &ret.Q)
+	ret.RSAMod = new(big.Int).Mul(ret.P, ret.Q)
 	fmt.Println("Bit length of RSAMod = ", ret.RSAMod.BitLen())
 	var ptemp, qtemp big.Int
-	ptemp.Sub(&ret.P, big1)
+	ptemp.Sub(ret.P, big1)
 	ptemp.Div(&ptemp, big2)
-	qtemp.Sub(&ret.Q, big1)
+	qtemp.Sub(ret.Q, big1)
 	qtemp.Div(&qtemp, big2)
-	ret.Order.Mul(&ptemp, &qtemp)
-	ret.Base = *getRanQR(&ret.P, &ret.Q)
+	ret.Order = new(big.Int).Mul(&ptemp, &qtemp)
+	ret.Base = getRanQR(ret.P, ret.Q)
 
-	var temp big.Int
+	var temp, big4 big.Int
+	big4.SetInt64(4)
+	ret.D = new(big.Int).SetInt64(publicKey)
+	ret.E = new(big.Int)
+	temp.GCD(ret.D, new(big.Int).Mul(ret.Order, &big4), ret.E, nil)
+	if temp.Cmp(new(big.Int).SetInt64(1)) != 0 {
+		fmt.Println("Error while setting up keys!")
+	}
+
 	temp.SetInt64(TimePara)
-	ret.Exponent.Exp(big2, &temp, &ret.Order)
+	ret.Exponent = new(big.Int).Exp(big2, &temp, ret.Order)
 	return &ret
 }
 
